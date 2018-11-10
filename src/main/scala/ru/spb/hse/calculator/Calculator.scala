db38e22c93adf497babf59f2ead9b953f48711a6
karvozavr <karvozavr@gmail.com>
2018-11-10 21:06:42 +0300

    Add an appropriate error handling.

--- src/main/scala/ru/spb/hse/calculator/Calculator.scala
+++ src/main/scala/ru/spb/hse/calculator/Calculator.scala
@@ -10,16 +10,16 @@ object Calculator {
 
   def main(args: Array[String]): Unit = {
     println("Arithmetic and logic evaluator:")
-    var input = readNextInput
+    var input = readNextInput()
 
     while (input.toLowerCase() != EXIT) {
       try {
         println(eval(input))
       } catch {
-        case _: Throwable => println("Incorrect expression.")
+        case e: ParserException => println(e.getMessage)
       }
 
-      input = readNextInput
+      input = readNextInput()
     }
   }
 
@@ -28,12 +28,28 @@ object Calculator {
 
   private def parse(expr: String): CalcContext = {
     val lexer = new ExpressionLexer(CharStreams.fromString(expr))
+    lexer.removeErrorListener(ConsoleErrorListener.INSTANCE)
+    lexer.addErrorListener(new ErrorListener())
     val parser = new ExpressionParser(new CommonTokenStream(lexer))
+    lexer.removeErrorListener(ConsoleErrorListener.INSTANCE)
+    parser.addErrorListener(new ErrorListener())
     parser.calc()
   }
 
-  private def readNextInput: String = {
+  private def readNextInput(): String = {
     print(prefix)
     scala.io.StdIn.readLine()
   }
+}
+
+private class ParserException(msg: String) extends RuntimeException(msg)
+
+private class ErrorListener extends BaseErrorListener {
+
+  override def syntaxError(recognizer: Recognizer[_, _],
+                           offendingSymbol: Any,
+                           line: Int,
+                           charPositionInLine: Int,
+                           msg: String, e: RecognitionException): Unit =
+    throw new ParserException(s"Parsing error at line $line:$charPositionInLine. Reason: $msg")
 }
